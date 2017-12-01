82707db2ec4a7621260737a0455ad00e2b58243b
niraj.jalan <niraj.jalan@gmail.com>
2017-12-01 11:05:33 -0500

    Added Euler problem 3 - Find greated prime factor

--- EulerMath.py
+++ EulerMath.py
@@ -1,5 +1,8 @@
 import time
+import math
+import sys
 
+sys.setrecursionlimit(2 * 10 ** 5)
 
 def findMultiplesOf3and5(number):
     sum = 0
@@ -20,8 +23,25 @@ def fibonacci(n):
 
     return sumValue
 
-
+def find_greatest_prime_factor(num):
+    
+    # base condition - 
+    if num < 3:
+      return num
+    
+    # find the sqrt
+    num_sqrt = math.ceil(math.sqrt(num))
+    #print(num_sqrt)
+    for i in xrange(int(num_sqrt), 1, -1):
+      if num%i == 0:
+        #check if i is prime
+        if find_greatest_prime_factor(i) == i:
+          return i
+    
+    return num
+  
 start_time = time.time()
 # findMultiplesOf3and5(1000)
-print(fibonacci(4000000))
+#print(fibonacci(4000000))
+print(find_greatest_prime_factor(32))
 print("This took", time.time() - start_time, "seconds to run.")
