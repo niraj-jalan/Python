cbde55f0fa2c764c1cc1217532831e49c807c947
niraj.jalan <niraj.jalan@gmail.com>
2017-12-01 12:02:31 -0500

    Optimised solution for Euler 3 problem - find largest prime factor

--- EulerMath.py
+++ EulerMath.py
@@ -9,7 +9,6 @@ def findMultiplesOf3and5(number):
     for i in range(1, number):
         if i % 3 == 0 or i % 5 == 0:
             sum = sum + i
-
     print(sum)
 
 
@@ -20,28 +19,40 @@ def fibonacci(n):
         a, b = b, a + b
         if b % 2 == 0:
             sumValue = sumValue + b
-
     return sumValue
 
+'''
+# This solution used sqrt function which takes addition logN time. The second solution to iterate over factor seems more efficient. 
 def find_greatest_prime_factor(num):
-    
+    #print(num)
     # base condition - 
     if num < 3:
       return num
-    
     # find the sqrt
     num_sqrt = math.ceil(math.sqrt(num))
-    #print(num_sqrt)
     for i in xrange(int(num_sqrt), 1, -1):
       if num%i == 0:
         #check if i is prime
         if find_greatest_prime_factor(i) == i:
           return i
-    
+    #num is prime - return num
     return num
-  
+'''
+def largest_prime_factor(num, div=2):
+    while div < num:
+        if num % div == 0 and num/div > 1:
+            num = num /div
+            div = 2
+        else:
+            div = div + 1
+    return num
+
+
 start_time = time.time()
 # findMultiplesOf3and5(1000)
 #print(fibonacci(4000000))
-print(find_greatest_prime_factor(32))
+print(largest_prime_factor(600851475143))
 print("This took", time.time() - start_time, "seconds to run.")
+#start_time = time.time()
+#print("This took", time.time() - start_time, "seconds to run.")
+
